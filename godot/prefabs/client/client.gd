23fa7ee1fb9d4b39d3bcd7c602c261acdea9c95f
Joaquin Muñiz <joaco.munix@gmail.com>
2024-01-28 15:48:50 -0300

    fila de clientes

--- godot/prefabs/client/client.gd
+++ godot/prefabs/client/client.gd
@@ -1,28 +1,60 @@
 class_name Client extends Node3D
 
+signal order_received
+
+@export var speed := 2.5
+
 @export var desired_object: ObjectData
 @export var phrase_override := ""
 
 @onready var label : Label3D = $Label
 @onready var object_pivot: Node3D = $Pivot
 
-func _ready():
-	ask_for_object()
+var can_interact = false
+var order_correct: bool
+
+func idle_chatter():
+	while true:
+		await get_tree().create_timer(randf_range(2, 60)).timeout
+		if can_interact: return
+		label.text = phrases_idle.pick_random()
+		await get_tree().create_timer(1.5).timeout
+		label.text = ""
 
 func ask_for_object():
+	can_interact = true
 	if not phrase_override.is_empty():
 		label.text = phrase_override
 	else:
 		label.text = "Gordo armame un %s, metele pata" % desired_object.name
 
 func give_object(obj: PickableObject):
+	if not can_interact:
+		return
+	
+	can_interact = false
 	obj.pick(object_pivot, true)
-	var is_correct = desired_object.tags.equals( obj.data.tags)
-	label.text = "Grasia ameo" if is_correct else "Que mierda es esto"
+	order_correct = desired_object.tags.equals( obj.data.tags)	
+	label.text = responses_right.pick_random() if order_correct else responses_wrong.pick_random()
 	await get_tree().create_timer(0.7).timeout
 	obj.queue_free()
-	await get_tree().create_timer(0.7).timeout
-	ask_for_object()
+	order_received.emit()
+	
+func leave(pos: Node3D):
+	say_exit_phrase()
+	await _move_to(pos)
+	queue_free()
+
+func say_exit_phrase():
+	await get_tree().create_timer(randf_range(1, 3)).timeout
+	label.text = phrases_afer_right.pick_random() if order_correct else phrases_feter_wrong.pick_random()
+	await get_tree().create_timer(1.5).timeout
+	label.text = ""
+
+func _move_to(pos: Node3D):
+	var tween = create_tween()
+	tween.tween_property(self, "global_position", pos.global_position, global_position.distance_to(pos.global_position) / speed)
+	await tween.finished
 
 func _on_body_entered(body):
 	var player = body as PlayerCharacter
@@ -33,3 +65,32 @@ func _on_body_exited(body):
 	var player = body as PlayerCharacter
 	if player and player.current_client == self: 
 		player.current_client = null
+		
+
+var phrases_idle = [
+	"Alta gula",
+	"Dale loco apuren",
+	"Aguante el Tomba",
+	"Fua, mirá el tamaño de esa rata"
+]
+
+var responses_right = [
+	"Grasia ameo",
+]
+
+var responses_wrong = [
+	"Que mierda es esto?",
+	"Esto no es lo que te pedí"
+]
+
+var phrases_afer_right = [
+	"Los mejores chori de Mendoza",
+	"Aguante el Tomba loco",
+	"Este chori tiene un gusto raro..."
+]
+
+var phrases_feter_wrong = [
+	"No vuelvo más acá",
+	"Una porqueria",
+	"Encima no reciben MercadoPago"
+]
---
+++ godot/prefabs/client_path.gd
@@ -0,0 +1,43 @@
+extends Node3D
+
+const client_template = preload("res://prefabs/client/client.tscn")
+
+@export var markers: Array[Node3D]
+@export var leave_pos: Node3D
+
+var clients: Array[Client]
+var current_client: Client
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for marker in markers:
+		var client = client_template.instantiate() as Client
+		add_child(client)
+		clients.push_back(client)
+		client.global_position = marker.global_position
+		client.idle_chatter()
+	
+	_next_client()
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _next_client():
+	if clients.is_empty():
+		return
+		
+	current_client = clients[0]
+	current_client.ask_for_object()
+	
+	await current_client.order_received
+	
+	current_client.leave(leave_pos)
+	clients.pop_front()
+	
+	var client_idx = 0
+	for client in clients:
+		await client._move_to( markers[client_idx])
+		client_idx += 1
+
+	_next_client()
